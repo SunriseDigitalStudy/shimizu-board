24f2682b7a0c2e2fd97c7820f693c01cdcddc4f4
shimizu-k-digi <shimizu-k@sunrisedigital.jp>
2015-06-16 15:59:46 +0900

    Validateの追加

--- board.sunrisedigital.jp/app/controllers/default/AccountController.php
+++ board.sunrisedigital.jp/app/controllers/default/AccountController.php
@@ -28,4 +28,55 @@ class AccountController extends Sdx_Controller_Action_Http
         $this->view->assign('account_list', $list);
         
     }
+    public function createAction()
+    {
+        $form = new Sdx_Form();
+        $form
+          ->setActionCurrentPage() //アクション先を現在のURLに設定
+          ->setMethodToPost(); //メソッドをポストに変更
+        
+        //各エレメントをフォームにセット
+//        login_id
+        $t_account = Bd_Orm_Main_Account::createTable();
+        $elem = new Sdx_Form_Element_Text();
+        $elem->setName('login_id');
+        $elem
+            ->setName('login_id')
+            ->addValidator(new Sdx_Validate_NotEmpty())
+            ->addValidator(new Sdx_Validate_Regexp('/^[a-zA-Z0-9]+$/u','英数字と＠_-のみ使用可能です'))
+            ->addValidator(new Sdx_Validate_Db_Unique($t_account,'login_id',$t_account
+                    ->getSelect()->forUpdate()));
+        $form->setElement($elem);
+        
+        //password
+        $elem = new Sdx_Form_Element_Password();
+        $elem
+            ->setName('password')
+            ->addValidator(new Sdx_Validate_NotEmpty())
+            ->addValidator(new Sdx_Validate_StringLength(array('min'=>4)));
+        $form->setElement($elem);
+        
+        //name
+        $elem = new Sdx_Form_Element_Text();
+        $elem
+            ->setName('name')
+            ->addValidator(new Sdx_Validate_NotEmpty())
+            ->addValidator(new Sdx_Validate_StringLength(array('max'=>18)));
+        $form->setElement($elem);
+        
+//        formがsubmitされていたら
+        if($this->_getParam('submit'))
+        {
+//            Validateを実行するためにformを値をセット
+//            エラーがあったとき各エレメントに値を戻す処理も兼ねている
+            $form->bind($this->_getAllParams());
+            
+//            Validateを実行
+            if($form->execValidate())
+            {
+//               すべてのエラーチェックを通過
+            }
+        }
+        $this->view->assign('form',$form);
+    }
 }
