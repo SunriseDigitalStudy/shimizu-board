dde5e91e44014fddab4a0d9e5d6c2dffc6f308ea
shimizu-k-digi <shimizu-k@sunrisedigital.jp>
2015-06-17 15:34:25 +0900

    ページの作成
    Scaffold
    ControlControllerクラスの作成

---
+++ board.sunrisedigital.jp/app/controllers/default/ControlController.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ControlController
+ *
+ * @author newpc
+ */
+class ControlController extends Sdx_Controller_Action_Http
+{
+    //put your code here
+    public function tagAction()
+    {
+        $this->_helper->scaffold->setViewRendererPath('default/control/scaffold.tpl');
+        $this->_helper->scaffold->run();
+    }
+}
