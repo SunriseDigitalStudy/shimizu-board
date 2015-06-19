ddfb0249dd7ab07b536ee91b4bb866c2fc0bed03
shimizu-k-digi <shimizu-k@sunrisedigital.jp>
2015-06-19 16:17:58 +0900

    SelectBuilderの実装
    ControlControllerとFooControllerでテスト

--- board.sunrisedigital.jp/app/controllers/default/ControlController.php
+++ board.sunrisedigital.jp/app/controllers/default/ControlController.php
@@ -11,13 +11,110 @@
  *
  * @author newpc
  */
-class ControlController extends Sdx_Controller_Action_Http
-{
+class ControlController extends Sdx_Controller_Action_Http {
+
     //put your code here
-    public function tagAction()
-    {
+    public function tagAction() {
 //      テンプレートのパスの設定  
         $this->_helper->scaffold->setViewRendererPath('default/control/scaffold.tpl');
         $this->_helper->scaffold->run();
     }
+
+    //    Sdx_Db_Select_Builder_Contextクラスをテストするためのアクション
+    public function selectbuilderAction() {
+        $this->_disableViewRenderer();
+//        作成するSQL
+//        SELECT
+//        `entry`.`id`,
+//        `entry`.`thread_id`,
+//        `entry`.`account_id`,
+//        `entry`.`body`,
+//        `entry`.`updated_at`,
+//        `entry`.`created_at`
+//        FROM
+//        `entry`
+//        WHERE
+//        (
+//        `entry`.`thread_id` = 1
+//        )
+//        AND (
+//        `entry`.`created_at` >= '2015-04-01 00:00:00'
+//        )
+//        ORDER BY
+//        `entry`.`created_at` DESC
+//        Entryテーブルの生成
+        $t_entry = Bd_Orm_Main_Entry::createTable();
+        $sb_entry = $t_entry->createSelectBuilder();
+
+//        Sdx_Db_Select_Builder_Context
+//        public function addWhere($column, $value, $comparison = Sdx_Db_Select::EQUAL) {
+//            return $this->add($column, $value, $comparison);
+//        }
+        $sb_entry
+                ->addWhere('thread_id', 1)
+                ->addWhere('create_at', '2015-04-01 00:00:00', Sdx_Db_Select::GREATER_EQUAL)
+//                比較演算子を省略した場合のテストコード
+//                ->addWhere('create_at', '2015-04-01 00:00:00')
+                ->order('create_at DESC');
+
+//      Sdx_Db_Selectの生成
+        $select = $sb_entry->build();
+
+//        SQLを発行しリストを取得
+        $list = $t_entry->fetchAll($select);
+    }
+
+//    Sdx_Db_Selectをテストするためのアクション
+    public function selectAction() {
+        $this->_disableViewRenderer();
+//        Entryテーブルの生成
+        $t_entry = Bd_Orm_Main_Entry::createTable();
+        $select = $t_entry->select();
+        $select
+                ->addWhere('thread_id', 1, $t_entry)
+                ->addWhere('create_at', '2015-04-01 00:00:00', $t_entry, Sdx_Db_Select::GREATER_EQUAL)
+//                order句を複数つける場合は複数回呼び出す
+                ->order($t_entry->appendAlias('create_at'));
+
+//        SQLを発行しリストを取得
+        $list = $t_entry->fetchAll($select);
+    }
+
+//    Sdx_Db_Select_Builder_Contextクラスを使用してJOINするテスト用アクション
+    public function joinbuilderAction() {
+        $this->_disableViewRenderer();
+//        Entryテーブルの生成
+        $t_entry = Bd_Orm_Main_Entry::createTable();
+        $sb_entry = $t_entry->createSelectBuilder();
+
+//        複数テーブルの一括JOIN
+        $sb_entry->thread->genre->innerJoinChain()
+                ->order('sequence DESC');
+
+        $sb_entry->thread->thread_tag->innerJoin()
+                ->addWhere('tag_id', array(2, 3));
+
+        $select = $sb_entry->build();
+
+//        SQLを発行しリストを取得
+        $list = $t_entry->fetchAll($select);
+    }
+
+//    Sdx_Db_Selectクラスを使用してJOINするテスト用アクション
+    public function joinselectAction() {
+        $this->_disableViewRenderer();
+        
+        $t_entry = Bd_Orm_Main_Entry::createTable();
+//        thread/genre両テーブルをJOIN。返り値には配列で書くSdx_Db_Tableのインスタンスが返る
+        list($t_thread,$t_genre) = $t_entry->innerJoin('Thread','Genre');
+        list($_thread_tag) = $t_thread->innerJoin('ThreadTag');
+        
+        $select = $t_entry->select();
+        $select
+           ->addWhere('tag_id',array(2,3),$_thread_tag)
+           ->order($t_genre->appendAlias('sequence DESC'));
+        
+        $list = $t_entry->fetchAll($select);
+    }
+
 }
--- board.sunrisedigital.jp/app/controllers/default/FooController.php
+++ board.sunrisedigital.jp/app/controllers/default/FooController.php
@@ -1,93 +1,89 @@
 <?php
 
-class FooController extends Sdx_Controller_Action_Http
-{
-    public function  indexAction()
-    {
+class FooController extends Sdx_Controller_Action_Http {
+
+    public function indexAction() {
         $this->_disableViewRenderer();
     }
 
-    public function barAction()
-  {
+    public function barAction() {
         $this->view->assign('date', date('Y-m-d H:i:s'));
-  }
-  
-  public function dbAction()
-  {
-      $this->_disableViewRenderer();
- 
-  //接続を取得
-  $db = Bd_Db::getConnection('board_master');
- 
-  //トランザクション開始
-  $db->beginTransaction();
- 
-  //テーブル名を指定してINSERT文を生成・実行
-  $db->insert('account', array(
-    'login_id' => 'check',
-    'password' => 'some_password',
-    'name' => 'Some Name',
-    'created_at' => date('Y-m-d H:i:s'),
-    'updated_at' => date('Y-m-d H:i:s'),
-  ));
- 
-  //コミット
-  $db->commit();
- 
-  //取得して確認
-  Sdx_Debug::dump($db->query("SELECT * FROM account")->fetchAll(), 'title');
-  }
-  
-  public function ormNewAction()
-  {
-      $this->_disableViewRenderer();
-      
+    }
+
+    public function dbAction() {
+        $this->_disableViewRenderer();
+
+        //接続を取得
+        $db = Bd_Db::getConnection('board_master');
+
+        //トランザクション開始
+        $db->beginTransaction();
+
+        //テーブル名を指定してINSERT文を生成・実行
+        $db->insert('account', array(
+            'login_id' => 'check',
+            'password' => 'some_password',
+            'name' => 'Some Name',
+            'create_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ));
+
+        //コミット
+        $db->commit();
+
+        //取得して確認
+        Sdx_Debug::dump($db->query("SELECT * FROM account")->fetchAll(), 'title');
+    }
+
+    public function ormNewAction() {
+        $this->_disableViewRenderer();
+
 //     レコードクラスの生成
-      $account = new Bd_Orm_Main_Account();
-      
-      $account
-        ->setLoginId('recodetest')
-        ->setName('Test Name')
-        ->setPassword('flkdjf0');
-      
+        $account = new Bd_Orm_Main_Account();
+
+        $account
+                ->setLoginId('newtest')
+                ->setName('New Name')
+                ->setPassword('flkdjf0');
+
 //      このレコードが使用する接続を取得
-      $db = $account->updateConnection();
-      
-      $db->beginTransaction();
-      $account->save();
-      $db->commit();
-      
+        $db = $account->updateConnection();
+
+        $db->beginTransaction();
+        $account->save();
+        $db->commit();
+
 //      取得して確認
-      Sdx_Debug::dump($db->query("SELECT * FROM account")->fetchAll(),"title");
-  }
-  public function ormSelectAction()
-  {
-      $this->_disableViewRenderer();
-      
+        Sdx_Debug::dump($db->query("SELECT * FROM account")->fetchAll(), "title");
+    }
+
+    public function ormSelectAction() {
+        $this->_disableViewRenderer();
+
 //      テーブルクラスの取得
-      $t_account = Bd_Orm_Main_Account::createTable();
-      
+        $t_account = Bd_Orm_Main_Account::createTable();
+
 //      主キー1のレコードを取得
-      $account =$t_account->findByPkey(1);
-      
+        $account = $t_account->findByPkey(1);
+
 //      toArray()はレコードの配列表現を取得するメソッド
-      Sdx_Debug::dump($account->toArray(),'title');
-      
+        Sdx_Debug::dump($account->toArray(), 'title');
+
 //      selectの取得
-      $select = $t_account->getselect();
-      
+        $select = $t_account->getselect();
+
 //      selectにWHERE句を追加
-      $select->add('id',array(6,8));
-      
+        $select->add('id', array(6, 8));
+
 //      sqlを発行
-      $list = $t_account->fetchAll($select);
-      
+        $list = $t_account->fetchAll($select);
+
 //      fetchAllの返り値は配列ではなくBd_Db_Record_Listのインスタンス
-      Sdx_Debug::dump($list,'title');
-      
+        Sdx_Debug::dump($list, 'title');
+
 //      Record_Listの配列表現をdump
-      Sdx_Debug::dump($list->toArray(),'title');
-      
+        Sdx_Debug::dump($list->toArray(), 'title');
+
 //      簡単なJOIN
 //      JOIN対象のテーブルを作成
       $t_account = Bd_Orm_Main_Account::createTable();
@@ -101,45 +97,45 @@ class FooController extends Sdx_Controller_Action_Http
       
 //      この結果はまだentryにレコードがないのでSQLだけ確認
       $list = $t_account->fetchAll($select);
-  }
-  public function  ormUpdateAction()
-  {
-      $this->_disableViewRenderer();
-      
+    }
+
+    public function ormUpdateAction() {
+        $this->_disableViewRenderer();
+
 //      テーブルクラスの取得
-      $t_account = Bd_Orm_Main_Account::createTable();
-      
+        $t_account = Bd_Orm_Main_Account::createTable();
+
 //      主キー1のレコードを更新
-      $account = $t_account->findByPKey(6);
-      
-      $account->setPassword('updated_password_'.date('Y-m-d H:i:s'));
-      
-      $db = $account->updateConnection();
-      
-      $db->beginTransaction();
-      $account->save();
-      $db->commit();
-      
+        $account = $t_account->findByPKey(6);
+
+        $account->setPassword('updated_password_' . date('Y-m-d H:i:s'));
+
+        $db = $account->updateConnection();
+
+        $db->beginTransaction();
+        $account->save();
+        $db->commit();
+
 //      取得して確認
-      Sdx_Debug::dump($db->query("select * from account")->fetchAll(),'title');
-  }
-  public function ormDeleteAction()
-  {
-      $this->_disableViewRenderer();
-      
+        Sdx_Debug::dump($db->query("select * from account")->fetchAll(), 'title');
+    }
+
+    public function ormDeleteAction() {
+        $this->_disableViewRenderer();
+
 //      テーブルクラスの取得
-      $t_account = Bd_Orm_Main_Account::createTable();
+        $t_account = Bd_Orm_Main_Account::createTable();
 //      主キー1のレコードを取得
-      $account = $t_account->findByPkey(6);
-      
-      $db = $account->updateConnection();
-      
-      $db->beginTransaction();
-      $account->delete();
-      $db->commit();
-      
+        $account = $t_account->findByPkey(4);
+
+        $db = $account->updateConnection();
+
+        $db->beginTransaction();
+        $account->delete();
+        $db->commit();
+
 //      取得して確認
-      Sdx_Debug::dump($db->query("select * from account")->fetchAll(),'title');
-  }
-  
+        Sdx_Debug::dump($db->query("select * from account")->fetchAll(), 'title');
+    }
+
 }
