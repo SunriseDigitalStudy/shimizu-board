a478c8eb210d376c2864c70ec826e2c86bb76ee8
shimizu-k-digi <shimizu-k@sunrisedigital.jp>
2015-07-09 15:33:05 +0900

    Scaffoldで作成したスレッド一覧のページングの動きを確認するためにコード修正

--- common/models/Bd/Orm/Main/Table/Thread.php
+++ common/models/Bd/Orm/Main/Table/Thread.php
@@ -3,16 +3,14 @@
 require_once 'Bd/Orm/Main/Base/Table/Thread.php';
 
 class Bd_Orm_Main_Table_Thread extends Bd_Orm_Main_Base_Table_Thread {
-  
-    public function fetchAllNewerOrdered(Sdx_Db_Select $select = null)
+
+  public function fetchAllNewerOrdered(Sdx_Db_Select $select = null)
     {
         if($select === null){
             $select = $this->getSelect();
         }
-//      orderby句 id降順の並び替え（ORDER BY id DESC）
-        $select->order('id DESC');
+        $select->order('id ASC');
         
         return $this->fetchAll($select);
     }
-    
 }
